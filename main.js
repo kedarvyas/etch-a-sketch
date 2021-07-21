6b750d6388e1e672e80031d36a77ebd39dc88260
kedarvyas <kedar.m.vyas@gmail.com>
2021-07-21 16:51:25 -0500

    updating main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const container = document.getElementById("main-container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.innerText = (c + 1);
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+  
+  makeRows(16, 16);
